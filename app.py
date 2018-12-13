cadbd30fe37b632aa204cbba38209a6bc5273a30
Sanya Nijhawan <sanya301@gmail.com>
2018-12-12 21:43:49 -0500

    converted to sql queries

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import os
 
 from flask import Flask, session, render_template, flash, request, redirect,\
-	url_for, jsonify
+        url_for
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy 
+from sqlalchemy import create_engine
 from forms import QueryForm
+import psycopg2
+import subprocess
+import json
+
 
 # Flask setup
 app = Flask(__name__)
@@ -29,10 +34,18 @@ if app.config["DEBUG"]:
         return response
 
 # Database object
-db = SQLAlchemy(app)
+proc = subprocess.Popen('heroku config:get DATABASE_URL -a wimpdb', stdout=subprocess.PIPE, shell=True)
+db_url = proc.stdout.read().decode('utf-8').strip() + '?sslmode=require'
 
-# Movie, Genre, Person, Appearance, Direction
-from models import *
+engine = create_engine(db_url)
+cur = engine.connect()
+
+def moviesByYear(yr):
+    movieList = cur.execute("""SELECT * FROM movies WHERE release_year=%s""", yr)
+    result = []
+    for row in movieList:
+        result.append(row)
+    return result
 
 # Bootstrap setup
 bootstrap = Bootstrap(app)
@@ -40,23 +53,8 @@ bootstrap = Bootstrap(app)
 # Homepage (form)
 @app.route("/", methods=['GET'])
 def index():
-    form = QueryForm(request.form)
-    return render_template('index.html', form=form)
-
-@app.route("/search", methods=["GET"])
-def search():
-    """
-    Endpoint for searchbox to query to get search suggestions.
-    """
-    if not request.args.get("q"):
-        raise RuntimeError("missing search query")
-
-    result = Person.query.filter(Person.name.ilike("%" + request.args.get("q")
-	    + "%")).all()
-    json = []
-    for person in result:
-        json.append({"name": person.name})
-    return jsonify(json)
+	form = QueryForm(request.form)
+	return render_template('index.html', form=form)
 
 # Form submission
 @app.route("/submit", methods=["POST"])
@@ -71,38 +69,18 @@ def process_form():
     # MVP of database operations
     # TODO: we should do the set operations in the DB queries, not Python!
     results = set()
+    movie_genres = None
 
     if start_year:
-	# Since these values are required, I guess we don't really need this check?
-        movies_in_year_range = Movie.query.filter(
-		Movie.release_year >= start_year,
-		Movie.release_year <= end_year).order_by(Movie.release_year)
-        results = set(movies_in_year_range)
-    if director:
-        movies_with_director = Movie.query.join(Direction,
-                Movie.id == Direction.movie_id).join(Person,
-                Direction.director_id == Person.id).filter(
-                        Person.name == director)
-        results &= set(movies_with_director)
-    if actor:
-        movies_with_actor = Movie.query.join(Appearance,
-		        Movie.id == Appearance.movie_id).join(Person,
-			        Appearance.actor_id == Person.id).filter(
-				            Person.name == actor)
-        results &= set(movies_with_actor)
-
-    if genres:
-        movies_of_genre = Movie.query.join(Genre,
-                Movie.id == Genre.movie_id).filter(Genre.genre.in_(genres))
-        results &= set(movies_of_genre)
-
-
+        # Since these values are required, I guess we don't really need this check?
+        results = moviesByYear(start_year)
+        
     # Render result in order of release year, alphabetical order within a year
-    sorted_results = sorted(list(results),
-	    key=lambda movie: (movie.release_year, movie.name))
+    sorted_results = sorted(results,
+            key=lambda movie: (movie[1], movie[2]))
     return render_template("result.html",
-	   message=f"",
-	   movies=enumerate(sorted_results, 1))
+           # message=f"",
+           movies=enumerate(sorted_results, 1))
 
 # Run Flask
 if __name__ == '__main__':
